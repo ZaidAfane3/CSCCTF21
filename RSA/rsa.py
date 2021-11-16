ac39e853a006dc096f8eda26392b5750c278ab6e
Hiexy <omarshuha@gmail.com>
2021-11-16 22:52:41 +0200

    RSA question

---
+++ RSA/rsa.py
@@ -0,0 +1,28 @@
+from Crypto.Util.number import *
+import os
+from Generator import generateKeys
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+e,n,d = generateKeys()
+priv = (d,n)
+pub = (e,n)
+
+def encrypt(pub, msg):
+        e, n = pub
+        i = bytes_to_long(msg.encode())
+        return int(pow(i, e, n))
+
+def decrypt(priv, cipher):
+        d, n = priv
+        return long_to_bytes(pow(cipher, d, n))
+
+
+
+with open('flag.txt', 'r') as f:
+        msg = f.read().strip()
+cipher = encrypt(pub, msg)
+
+print(f'e = {pub[0]}\nn = {pub[1]}')
+print('-' * 185)
+print(f'c = {cipher}')
+print('-' * 185)
