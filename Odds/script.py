b7f167933693824510bb7ee9d4c14f1f7c9c072c
Hiexy <omarshuha@gmail.com>
2021-11-17 21:02:38 +0200

    Odds Done

---
+++ Odds/script.py
@@ -0,0 +1,29 @@
+import random
+
+odds = ['H','T']
+
+init = random.choice(odds)
+
+print('This should be easy.')
+
+score = 0
+while True:
+    print('[H]eads or [T]ails')
+    choice = input('> ')
+    if init.lower() == choice.lower():
+        print('Nice')
+        score += 1
+    else:
+        print('Wrong')
+        exit(1)
+
+    if init == 'H':
+        init = 'T'
+    elif init == 'T':
+        init = 'H'
+    
+    if score == 500:
+        with open('flag.txt', 'r') as f:
+            print(f.read())
+            exit()
+    
