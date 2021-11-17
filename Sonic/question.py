f40c6e8a9ad7f2fc71d9a28331930ced10845f5c
Hiexy <omarshuha@gmail.com>
2021-11-17 10:59:47 +0200

    Sonic Done

---
+++ Sonic/question.py
@@ -0,0 +1,46 @@
+import random
+import base64
+from Crypto.Util.number import *
+
+with open('wordlist.txt', 'r') as f:
+    ls = [line.strip() for line in f.readlines()]
+
+chall = ['base64', 'rot', 'hex', 'long']
+
+def rot_alpha(n):
+    from string import ascii_lowercase as lc, ascii_uppercase as uc
+    lookup = str.maketrans(lc + uc, lc[n:] + lc[:n] + uc[n:] + uc[:n])
+    return lambda s: s.translate(lookup)
+
+print('Quick I need you to decode the following messages.')
+print('If you can help me, I\'ll give you a prize!')
+
+score = 0
+while True:
+    plaintxt = "_".join(random.choices(ls, k=4))
+    encoding = random.choice(chall)
+    if encoding == 'base64':
+        print(encoding,base64.b64encode(plaintxt.encode()).decode(),sep=':')
+
+    if encoding == 'rot':
+        n = random.randint(1, 25)
+        print(encoding,rot_alpha(n)(plaintxt), n,sep=':')
+
+    if encoding == 'hex':
+        print(encoding,plaintxt.encode().hex(),sep=':')
+
+    if encoding == 'long':
+        print(encoding,bytes_to_long(plaintxt.encode()),sep=':')
+
+    decoded = input('> ')
+    if decoded == plaintxt:
+        score += 1
+    else:
+        print('WRONG DECODED MESSAGE!')
+        exit(1)
+
+    if score == 200:
+        with open('flag.txt', 'r') as f:
+            print('Congratulations, For your assistance, you will be rewarded with a flag!!')
+            print(f.read())
+            exit()
