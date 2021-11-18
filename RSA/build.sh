d64f1e60b333f0201b38ae8732f1312119671ddc
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 21:50:18 +0200

    Fix Docker RSA

--- RSA/build.sh
+++ RSA/build.sh
@@ -3,4 +3,4 @@
 docker rm -f rsa 
 docker build -t rsa:latest . 
 docker run -d --name rsa  -p 10456:1234 -it rsa:latest
-docker exec -d -it rsa bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/rsa.py"
+docker exec -d -it rsa bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:'/bin/bash /app/script.sh'"
