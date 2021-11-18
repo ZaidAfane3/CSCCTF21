2f279f65cbaee48a24ebb6a7b75843a2a2daeb4f
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 21:49:11 +0200

    Fix Docker Odds

--- Odds/build.sh
+++ Odds/build.sh
@@ -3,4 +3,4 @@
 docker rm -f odds 
 docker build -t odds:latest . 
 docker run -d --name odds  -p 10454:1234 -it odds:latest
-docker exec -d -it odds bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/script.py"
+docker exec -d -it odds bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:'/bin/bash /app/script.sh'"
