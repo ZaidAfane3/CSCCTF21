ea68bf4b2b4e186d0bc1a319d45633e653ac6a0c
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 16:41:00 +0200

    Docker BFKey

---
+++ BFKey/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+docker rm -f bfkey 
+docker build -t bfkey:latest . 
+docker run -d --name bfkey  -p 10452:1234 -it bfkey:latest
+docker exec -d -it bfkey bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/main.py"
