edceea9a576f9ad05c71419760a9044a26844a24
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 16:29:40 +0200

    Docker Calculator

---
+++ Calculator/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+docker rm -f calculator 
+docker build -t calculator:latest . 
+docker run -d --name calculator  -p 10451:1234 -it calculator:latest
+docker exec -d -it calculator bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/calc.py"
