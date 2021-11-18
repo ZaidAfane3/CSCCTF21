d083248bd65837b5583d6594dee7c29876157690
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 16:56:20 +0200

    Docker Rsa

---
+++ RSA/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+docker rm -f rsa 
+docker build -t rsa:latest . 
+docker run -d --name rsa  -p 10454:1234 -it rsa:latest
+docker exec -d -it rsa bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/rsa.py"
