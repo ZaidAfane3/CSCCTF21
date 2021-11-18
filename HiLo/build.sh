dbeef4d3c87fdfa5c39842bf843de399db79f49f
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 16:54:17 +0200

    Fix HiLo Build.sh

--- HiLo/build.sh
+++ HiLo/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-docker rm -f collisions 
-docker build -t collisions:latest . 
-docker run -d --name collisions  -p 10455:1234 -it collisions:latest
-docker exec -d -it collisions bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/script.py"
+docker rm -f hilo 
+docker build -t hilo:latest . 
+docker run -d --name hilo  -p 10455:1234 -it hilo:latest
+docker exec -d -it hilo bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/script.py"
