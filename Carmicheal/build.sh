34ef8cbee2e89d6684628bc6aabb8a8f3c8baaf6
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 16:42:18 +0200

    Fix build.sh Carmicheal

--- Carmicheal/build.sh
+++ Carmicheal/build.sh
@@ -2,5 +2,5 @@
 
 docker rm -f carmicheal 
 docker build -t carmicheal:latest . 
-docker run -d --name carmicheal  -p 10451:1234 -it carmicheal:latest
+docker run -d --name carmicheal  -p 10453:1234 -it carmicheal:latest
 docker exec -d -it carmicheal bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/carmicheal.py"
