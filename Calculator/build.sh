e80ced11ba619efde351421bd2ef09ac0ad597ef
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 21:33:07 +0200

    Fix Docker Calc

--- Calculator/build.sh
+++ Calculator/build.sh
@@ -3,4 +3,4 @@
 docker rm -f calculator 
 docker build -t calculator:latest . 
 docker run -d --name calculator  -p 10451:1234 -it calculator:latest
-docker exec -d -it calculator bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/calc.py"
+docker exec -d -it calculator bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:'/bin/bash /app/script.sh'"
