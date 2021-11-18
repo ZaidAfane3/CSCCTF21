c1101544450156306efcc5b3b1199bc4f5610d22
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 21:31:11 +0200

    Fix Docker BFkey

--- BFKey/build.sh
+++ BFKey/build.sh
@@ -3,4 +3,4 @@
 docker rm -f bfkey 
 docker build -t bfkey:latest . 
 docker run -d --name bfkey  -p 10452:1234 -it bfkey:latest
-docker exec -d -it bfkey bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/main.py"
+docker exec -d -it bfkey bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:'/bin/bash /app/script.sh'"
