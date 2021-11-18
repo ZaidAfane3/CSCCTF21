716d5e41b3ec7df2f6e6d0e8ff8232f3cf65a246
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 21:51:08 +0200

    Fix Docker Sonic

--- Sonic/build.sh
+++ Sonic/build.sh
@@ -3,4 +3,4 @@
 docker rm -f rsa 
 docker build -t rsa:latest . 
 docker run -d --name rsa  -p 10457:1234 -it rsa:latest
-docker exec -d -it rsa bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/question.py"
+docker exec -d -it rsa bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:'/bin/bash /app/script.sh'"
