5472742570e50c79af005a5e3ecdc126028f89e6
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 16:54:24 +0200

    Docker Odds

---
+++ Odds/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+docker rm -f odds 
+docker build -t odds:latest . 
+docker run -d --name odds  -p 10454:1234 -it odds:latest
+docker exec -d -it odds bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/script.py"
