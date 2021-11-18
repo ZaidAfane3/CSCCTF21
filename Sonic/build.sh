cecb4e649ac5b787940a95a5eed50a8577aa2dc6
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-19 00:51:58 +0200

    Fix build.sh Sonic

--- Sonic/build.sh
+++ Sonic/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-docker rm -f rsa 
-docker build -t rsa:latest . 
-docker run -d --name rsa  -p 10457:1234 -it rsa:latest
-docker exec -d -it rsa bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:'/bin/bash /app/script.sh'"
+docker rm -f sonic 
+docker build -t sonic:latest . 
+docker run -d --name sonic  -p 10457:1234 -it sonic:latest
+docker exec -d -it sonic bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:'/bin/bash /app/script.sh'"
