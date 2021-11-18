10c79febb8621d8b5e146dc91d5abb148404f78b
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 21:48:04 +0200

    Fix Docker

--- HiLo/build.sh
+++ HiLo/build.sh
@@ -3,4 +3,4 @@
 docker rm -f hilo 
 docker build -t hilo:latest . 
 docker run -d --name hilo  -p 10455:1234 -it hilo:latest
-docker exec -d -it hilo bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/script.py"
+docker exec -d -it hilo bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:'/bin/bash /app/script.sh'"
