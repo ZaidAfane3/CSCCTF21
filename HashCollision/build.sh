94d21f184f976264c70aeedb2beac5c89fc52aa6
Hiexy <45509563+Hiexy@users.noreply.github.com>
2021-11-18 16:23:26 +0200

    Fix build.sh Collisions

--- HashCollision/build.sh
+++ HashCollision/build.sh
@@ -2,5 +2,5 @@
 
 docker rm -f collisions 
 docker build -t collisions:latest . 
-docker run -d --name collisions  -p 4000:1234 -it collisions:latest
+docker run -d --name collisions  -p 1932:1234 -it collisions:latest
 docker exec -d -it collisions bash -c "/usr/bin/socat TCP-LISTEN:1234,fork,reuseaddr EXEC:/app/hashcol.py"
